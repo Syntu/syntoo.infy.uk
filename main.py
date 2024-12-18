156369d45843e55f037897bb415d1206662c86e0
Syntu <66807482+Syntu@users.noreply.github.com>
2024-12-18 05:52:35 +0545

    main.py

--- main.py
+++ main.py
@@ -89,10 +89,10 @@ async def handle_stock_symbol(update: Update, context: ContextTypes.DEFAULT_TYPE
             f"Turnover: {data['Turnover']}"
         )
     else:
-        response = f"Symbol '{symbol}'
+        response = f"""Symbol '{symbol}'
         लौ जा, फेला परेन त 🤗🤗।
         कि Symbol को Spelling मिलेन ?
-        अझै Try गर्नुस।"
+        अझै Try गर्नुस।"""
     await update.message.reply_text(response, parse_mode=ParseMode.HTML)
 
 # Main function to set up the bot and run polling
