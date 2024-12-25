40631d891cf375be9359719a5f16a60986d1870c
Syntu <66807482+Syntu@users.noreply.github.com>
2024-12-25 09:22:20 +0545

    main.py

--- main.py
+++ main.py
@@ -385,7 +385,7 @@ def upload_to_ftp(html_content):
         f.write(html_content)
     with ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS) as ftp:
         ftp.cwd("/htdocs")
-        with open("index.html", "rb") as f):
+        with open("index.html", "rb") as f:
             ftp.storbinary("STOR index.html", f)
 
 # Refresh Data
